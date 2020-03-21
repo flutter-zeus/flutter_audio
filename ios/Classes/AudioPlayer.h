4e48a5c53679130d917fec7e5ab1646109389b3c
flutter-zeus <flutter.zeus@gmail.com>
2020-03-21 13:09:04 +0900

    fix bug not change voice

---
+++ ios/Classes/AudioPlayer.h
@@ -0,0 +1,24 @@
+#import <Flutter/Flutter.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface AudioPlayer : NSObject
+
+@property NSString* uid;
+@property FlutterMethodChannel* channel;
+@property NSString* lastUrl;
+@property AVPlayer* player;
+@property AVPlayerItem* playerItem;
+@property bool preloaded;
+
+- (id)initWithUid: (NSString*) uid channel:(FlutterMethodChannel*)channel;
+- (void)play: (NSString*) url;
+- (void)preload: (NSString*) url;
+- (void)pause;
+- (void)stop: (bool) completed;
+- (void)seek: (double) position;
+- (void)volume: (float) volume;
+- (void)releasePlayer;
+
+- (int)getPlayerDuration;
+
+@end
